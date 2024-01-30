a895d04b15974351e8d5115f6301a86f8445c448
Muna0027 <adan0027@algonquinlive.com>
2024-01-29 19:09:26 -0500

    Load the application config information

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -1,1 +1,14 @@
 import * as pulumi from "@pulumi/pulumi";
+
+// Import the configuration settings for the current stack.
+const config = new pulumi.Config()
+const appPath = config.get('appPath') || '../'
+const prefixName = config.get('prefixName') || 'cst8918-a03-student'
+const imageName = prefixName
+const imageTag = config.get('imageTag') || 'latest'
+// Azure container instances (ACI) service does not yet support port mapping
+// so, the containerPort and publicPort must be the same
+const containerPort = config.getNumber('containerPort') || 80
+const publicPort = config.getNumber('publicPort') || 80
+const cpu = config.getNumber('cpu') || 1
+const memory = config.getNumber('memory') || 2
